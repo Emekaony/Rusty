118aa2864ff0a086fb80cb66ccd8db4338b064fb
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-10-22 02:29:20 -0700

    still get the fundamentals, need to lock in for lifetimes & async stuff.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+#[derive(Debug)]
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+fn display_person_attributes(p: &Person) -> String {
+    let message: String = format!("hello there {} {}", p.first_name, p.last_name);
+    return message;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let emeka = Person {
+        first_name: "Nnaemeka".to_string(),
+        last_name: "Onyeokoro".to_string(),
+    };
+    println!("{}", display_person_attributes(&emeka));
+    println!("emeka's first name is {}.", emeka.first_name);
+    println!("emeka's last name is {}.", emeka.last_name);
 }
