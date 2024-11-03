eba8f05ead936690a99741cf0c53417390e4d2ff
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-11-02 20:44:16 -0700

    Used impl keyword to implement functions for a structure.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
-use rand::{rngs::ThreadRng, Rng};
-mod player;
-
-fn main() {
-    // trying to generate random number
-    let mut rng: ThreadRng = rand::thread_rng();
-    let num: i32 = rng.gen();
-    let random_number_between_10_and_20: i32 = rng.gen_range(10..20);
-    println!("ranodm number: {}", num);
-    println!(
-        "Random number between 10 and 20 is {}",
-        random_number_between_10_and_20
-    );
+#[derive(Debug)]
+struct Employee {
+    firstname: String,
+    company: String,
+    age: u32,
 }
 
-// modules are not the same as folders!!!!!!!!!!
-mod clean {
-    #[allow(dead_code)]
-    fn perform_cleanup_private() {
-        println!("Cleaning private");
+impl Employee {
+    fn details(&self) -> String {
+        format!(
+            "name: {}, age: {}, company: {}",
+            self.firstname, self.age, self.company
+        )
     }
-    #[allow(dead_code)]
-    pub fn perform_cleanup_public() {
-        println!("Cleaning up public");
-    }
+}
+
+fn main() {
+    let emp: Employee = Employee {
+        firstname: "Nnaemeka".to_string(),
+        company: "Rhombus".to_string(),
+        age: 23,
+    };
+    println!("{}", emp.details());
+    println!("{:?}", emp);
 }
