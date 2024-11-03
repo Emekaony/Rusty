39a535ab31c8ba6fdd64707dfdc4e39e98b161bd
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2024-11-02 20:22:05 -0700

    Still learning the basics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+use rand::{rngs::ThreadRng, Rng};
+
+use crate::archive::arch::arch_file;
+
+mod archive;
+mod player;
+
 fn main() {
-    let message: &str = "Break down functionality into different components";
-    for _ in 0..10 {
-        println!("{}", message)
+    // trying to generate random number
+    let mut rng: ThreadRng = rand::thread_rng();
+    let num: i32 = rng.gen();
+    let random_number_between_10_and_20: i32 = rng.gen_range(10..20);
+    println!("ranodm number: {}", num);
+    println!(
+        "Random number between 10 and 20 is {}",
+        random_number_between_10_and_20
+    );
+}
+
+// modules are not the same as folders!!!!!!!!!!
+mod clean {
+    #[allow(dead_code)]
+    fn perform_cleanup_private() {
+        println!("Cleaning private");
+    }
+
+    pub fn perform_cleanup_public() {
+        println!("Cleaning up public");
     }
 }
---
+++ src/player.rs
@@ -0,0 +1,15 @@
+use crate::clean;
+
+pub fn play_movie(name: &str) {
+    println!("Playing movie {}", name);
+    clean::perform_cleanup_public();
+}
+#[allow(dead_code)]
+fn play_audio(name: &str) {
+    println!("Playing audio {}", name)
+}
+
+#[allow(dead_code)]
+pub fn new_stuff() {
+    println!("He said the file is now a moudle?");
+}
